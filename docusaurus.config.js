18530c3c84a0234d61ded4ebf582d31c2b917b64
Archie <archie@fydeos.io>
2023-08-30 17:36:43 +0800

    add edit this page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,11 +58,11 @@ const config = {
                     routeBasePath: '/',
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/Lorde627/fydetab-wiki-dev/tree/main/',
                 },
                 blog: {
                     showReadingTime: true,
-                    // Please change this to your repo.
-                    // Remove this to remove the "edit this page" links.
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
